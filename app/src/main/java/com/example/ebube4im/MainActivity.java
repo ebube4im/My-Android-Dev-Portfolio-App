c15f5b23854f2fac950289b78d3ac3234e1c7284
ebube4im <ebube4im@gmail.com>
2022-02-01 23:48:03 +0100

    Initial Day 1 Commit

---
+++ app/src/main/java/com/example/ebube4im/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ebube4im;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // This Application is My Portfolio Project for Android where I will showcase All I am learning with Android Studio
+    // This project is to help be become an expert with using Java for Android Dev.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
